1b95ff3d84156482e471d1fec324c429910cb7e3
HarryWu-06 <78174007+HarryWu-06@users.noreply.github.com>
2022-06-09 09:06:07 -0400

    Add files via upload
    Version 1

---
+++ The Last Soul - Java Project Version 1 (Harry and Huda) - 20220608/The Last Soul - Java Final Project Version 1 (Harry and Huda)/The Last Soul/src/GameFrame.java
@@ -0,0 +1,20 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class GameFrame extends JFrame{
+	private ImageIcon gameIcon;
+	
+	public GameFrame(GamePanel gamePanel){
+		gameIcon = new ImageIcon("images\\gameIcon.png");
+		this.setIconImage(gameIcon.getImage());
+		
+		this.setTitle("The Last Soul");
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.add(gamePanel);
+		this.pack();
+		this.setResizable(false);
+		this.setLocationRelativeTo(null);
+		
+		this.setVisible(true);
+	}
+}
