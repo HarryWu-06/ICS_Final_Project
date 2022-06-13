152b423c223ca76e0e1d976136a3988d752e1b92
hudahameed <74416806+hudahameed@users.noreply.github.com>
2022-06-12 23:30:33 -0400

    Add files via upload

--- Thelastsoul/src/SettingPage.java
+++ Thelastsoul/src/SettingPage.java
@@ -13,19 +13,20 @@ import javax.swing.JTextField;
 
 public class SettingPage extends JFrame implements ActionListener{
 	ImageIcon gameIcon;
-	JFrame frame = new JFrame();
 	Container con;
 	JPanel textPanel, inputPanel;
 	JLabel textLabel;
 	Font normalFont = new Font("Times New Roman", Font.PLAIN, 26);
 	JTextField jtf;
-	JButton skinB, gametime, bgmusic;
+	JButton skinB, gametime, bgmusic, back, game;
 	JLabel titleS;
 	JLabel authorS;
 	
 	Color green = new Color(43, 220, 20);
 	
 	public SettingPage() {
+		JFrame frame = new JFrame("Settings!");
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		gameIcon = new ImageIcon("images\\gameIcon.png");
 		this.setIconImage(gameIcon.getImage());
 		
@@ -46,23 +47,32 @@ public class SettingPage extends JFrame implements ActionListener{
 		bgmusic = new JButton();
 		bgmusic.setText("change background music");
 		bgmusic.setFont(new Font("MV Boli", Font.BOLD, 12));
-		bgmusic.setBounds(500, 300, 250, 60);
+		bgmusic.setBounds(100, 350, 250, 60);
 		bgmusic.setFocusable(false);
 		bgmusic.setForeground(Color.black);
 		
 		skinB = new JButton();
 		skinB.setText("change character/weapon skin");
 		skinB.setFont(new Font("MV Boli", Font.BOLD, 12));
-		skinB.setBounds(500, 380, 250, 60);
+		skinB.setBounds(350, 350, 250, 60);
 		skinB.setFocusable(false);
 		skinB.setForeground(Color.black);
 
 		
 		gametime = new JButton("check game time");
 		gametime.setFont(new Font("MV Boli", Font.BOLD, 18));
-		gametime.setBounds(500, 460, 250, 60);
+		gametime.setBounds(600, 350, 250, 60);
 		gametime.setFocusable(false);
 		gametime.setForeground(Color.black);
+		
+		back = new JButton("Back");
+		back.setBounds(100, 300, 80, 40);
+		back.setFocusable(false);
+		back.addActionListener(this);
+		game = new JButton("Go to game");
+		game.setBounds(650, 410, 200, 50);
+		game.setFocusable(false);
+		game.addActionListener(this);
 		
 		titleS = new JLabel("Setting page");
 		titleS.setFont(new Font("MV Boli", Font.BOLD, 95));
@@ -71,15 +81,13 @@ public class SettingPage extends JFrame implements ActionListener{
 		authorS.setFont(new Font("MV Boli", Font.BOLD, 30));
 		authorS.setBounds(80, 150, 700, 100);
 		
-		frame.add(skinB);
-		frame.add(gametime);
-		frame.add(bgmusic);
-		
 		this.add(bgmusic);
 		this.add(skinB);
 		this.add(gametime);
 		this.add(titleS);
 		this.add(authorS);
+		this.add(back);
+		this.add(game);
 		this.setVisible(true);
 		this.getContentPane().setBackground(Color.pink);
 		
@@ -94,10 +102,21 @@ public class SettingPage extends JFrame implements ActionListener{
 		}
 		else if(e.getSource() == skinB) {
 			new Skin(); 
+			this.dispose();
 		}
 		else if(e.getSource() == gametime) { 
 			new Gametime();
+			this.dispose();
 		}
+		else if(e.getSource() == back) {
+			new TitlePage(); 
+			this.dispose();
+		}
+		else if(e.getSource() == game) {
+			new Game();
+			this.dispose();
+		}
+		
 		
 	}
 
